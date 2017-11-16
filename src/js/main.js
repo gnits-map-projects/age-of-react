48d3cce020fa73927584a46389d0d522d40dbb64
Arun Karunagath <the1.arun@gmail.com>
2017-11-16 19:33:14 +0100

    Workspace

--- src/js/main.js
+++ src/js/main.js
@@ -1,1 +1,34 @@
+function step1() {
+    ReactDOM.render("Hurray", document.getElementById('root'));
+}
 
+
+function step2() {
+    const aLink = React.createElement(
+        'a',
+        {
+            className: 'btn btn-primary btn-lg',
+            href: '#',
+            role: 'button',
+        },
+        'Learn more >>'
+    );
+
+    const aPara = React.createElement('p', {}, aLink);
+
+    ReactDOM.render(aPara, document.getElementById('lean-more'));
+}
+
+function step3() {
+    const jumbotron = null;
+
+    ReactDOM.render(
+        jumbotron,
+        document.getElementById('new-header')
+    );
+}
+
+
+// step1();
+// step2();
+// step3();
