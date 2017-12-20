b503c24e62d9703833335365aa96b4ec8b42dd1b
Arun Karunagath <the1.arun@gmail.com>
2017-12-20 19:25:19 +0530

    example of using ref -> uncontrolled component

---
+++ src/components/ContactForm.jsx
@@ -0,0 +1,40 @@
+import React from 'react';
+
+
+class ContactForm extends React.Component {
+    constructor (props) {
+        super(props);
+        this.onSubmit = this.onSubmit.bind(this);
+    }
+
+    onSubmit (event) {
+        console.log(event, this.name.value);
+        event.preventDefault();
+    }
+
+    render () {
+        return (
+            <div>
+                <h2>Contact me:</h2>
+                <form onSubmit={this.onSubmit}>
+                    <div className="form-group">
+                        <label htmlFor="name">Name</label>
+                        <input type="text" className="form-control" id="name" placeholder="Enter name" ref={(el) => { this.name = el; } } />
+                    </div>
+                    <div className="form-group">
+                        <label htmlFor="email">Email</label>
+                        <input type="email" className="form-control" id="email" aria-describedby="emailHelp" placeholder="Enter email" />
+                        <small id="emailHelp" className="form-text text-muted">We'll never share your email with anyone else.</small>
+                    </div>
+                    <div className="form-group">
+                        <label htmlFor="message">Short message</label>
+                        <textarea className="form-control" id="message" rows="3"></textarea>
+                    </div>
+                    <button type="submit" className="btn btn-primary">Submit</button>
+                </form>
+            </div>
+        );
+    }
+};
+
+export default ContactForm;
