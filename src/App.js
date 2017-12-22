cd6f5dfea17fb12de0100b57a9a2d6a9d990077d
Arun Karunagath <the1.arun@gmail.com>
2017-12-22 15:11:39 +0530

    draw a line

--- src/App.js
+++ src/App.js
@@ -1,25 +1,46 @@
 import React from 'react';
 import PropTypes from 'prop-types';
-import { Map, Marker, Popup, TileLayer } from 'react-leaflet'
+import { Map, Marker, TileLayer, Polyline } from 'react-leaflet'
+import request from 'superagent';
 
-const App = (props) => (
-  <Map center={props.position} zoom={15}>
-    <TileLayer
-      url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
-      attribution="&copy; <a href=&quot;http://osm.org/copyright&quot;>OpenStreetMap</a> contributors"
-    />
-    <Marker position={props.position}>
-      <Popup>
-        <span>
-          Hej, we are here!<br /> Now where is Charminar?.
-        </span>
-      </Popup>
-    </Marker>
-  </Map>
-);
+class App extends React.Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      user: "",
+      locations: {
+        "gnit": [17.4118166, 78.3967619],
+        "charminar": [17.3615636, 78.4724758],
+      }
+    };
+  }
+
+  componentDidMount() {
+    const promise = request.get("https://api.github.com/users/kra3");
+    const onSucess = (val) => { this.setState({ user: val.body.login })};
+    promise.then(onSucess);
+  }
+
+  render() {
+    return (
+      <Map center={this.props.mapCenter} zoom={12}>
+        <TileLayer
+          url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
+          attribution="&copy; <a href=&quot;http://osm.org/copyright&quot;>OpenStreetMap</a> contributors"
+        />
+        {
+          Object.keys(this.state.locations).map(
+            (location) => <Marker key={location} position={this.state.locations[location]} />)
+        }
+        <Polyline positions={Object.values(this.state.locations)} />
+      </Map>
+    );
+  }
+};
 
 App.propTypes = {
-  position: PropTypes.arrayOf(PropTypes.number).isRequired,
+  mapCenter: PropTypes.arrayOf(PropTypes.number).isRequired,
 }
 
 export default App;
--- src/index.js
+++ src/index.js
@@ -4,7 +4,6 @@ import './index.css';
 import App from './App';
 import registerServiceWorker from './registerServiceWorker';
 
-const gnit = [17.4118166,78.3967619]
 
-ReactDOM.render(<App position={gnit} />, document.getElementById('root'));
+ReactDOM.render(<App mapCenter={[17.3943459,78.4244423]} />, document.getElementById('root'));
 registerServiceWorker();
